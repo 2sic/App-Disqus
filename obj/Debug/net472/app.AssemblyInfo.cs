65838e227e320ba6356d61c1a03b44656da3348d
Dominik Graf <dominik.graf@2sic.com>
2024-04-05 07:52:47 +0200

    minor

--- obj/Debug/net472/app.AssemblyInfo.cs
+++ obj/Debug/net472/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79981374b64e53eef4f2cb8c8543532d4703e81f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edd2dfa30b7a02918ac5a2c5ec6cada7451ffb67")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
